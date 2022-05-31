d083297847e4ac012a5268abb2aab04ad155d248
Ben <kaleidawave@gmail.com>
2022-05-31 17:21:10 +0100

    Doesn't like double ampersand I think

--- benchmarks/rocket-bench/run.sh
+++ benchmarks/rocket-bench/run.sh
@@ -1,16 +1,16 @@
-brew install wrk &&
+brew install wrk
 
-rustup toolchain install nightly &&
-rustup default nightly &&
+rustup toolchain install nightly
+rustup default nightly
 
-cargo build --timings=html &&
+cargo build --timings=html
 
-mv target/cargo-timings/* artifacts &&
+mv target/cargo-timings/* artifacts
 
 ./target/debug/rocket-bench &
 
 # pause for server to startup
-sleep 5
+sleep 10
 
 wrk -t12 -c200 -d30s http://127.0.0.1:3000
 wrk -t12 -c200 -d30s http://127.0.0.1:3000 > artifacts/wrk.output.txt
