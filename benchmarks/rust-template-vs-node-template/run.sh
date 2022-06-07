84e21e3a2a817310fa1f41fe68b395d8418df912
Ben <kaleidawave@gmail.com>
2022-06-07 14:19:19 +0100

    Add extension when refering to action

--- benchmarks/rust-template-vs-node-template/run.sh
+++ benchmarks/rust-template-vs-node-template/run.sh
@@ -4,4 +4,4 @@ brew install hyperfine
 # Rust 
 cargo build --release
 
-hyperfine "./target/release/main" "node index.js"
+hyperfine "./target/release/main" "node index.js" > "$ARTIFACTS_FOLDER/hyperfine.output.1.txt"
