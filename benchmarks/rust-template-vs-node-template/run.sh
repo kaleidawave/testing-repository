b590f79b30084a405a152dbef0aeeed7afd5d198
Ben <kaleidawave@gmail.com>
2022-06-07 14:32:13 +0100

    Push to run action

--- benchmarks/rust-template-vs-node-template/run.sh
+++ benchmarks/rust-template-vs-node-template/run.sh
@@ -1,7 +1,9 @@
 # Installation
 brew install hyperfine
 
-# Rust 
+# Rust setup
 cargo build --release
 
-hyperfine "./target/release/main" "node index.js" > "$ARTIFACTS_FOLDER/hyperfine.output.1.txt"
+ls -R target > "$ARTIFACTS_FOLDER/ls.txt"
+
+hyperfine "./target/release/main" "node index.js" > "$ARTIFACTS_FOLDER/hyperfine.output.txt"
