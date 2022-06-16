4fc5a804c9778dc62043e67b6a9ebf5d72256587
Ben <kaleidawave@gmail.com>
2022-06-16 15:40:23 +0100

    в fixes

--- benchmarks/rust-threads-vs-async/main.rs
+++ benchmarks/rust-threads-vs-async/main.rs
@@ -18,7 +18,7 @@ fn main() {
     }
 
     let mut handles = Vec::new();
-    for i in 1..100 {
+    for _ in 1..100 {
         if cfg!(feature = "threads") {
             let handle = std::thread::spawn(sync_connect_and_read);
             handles.push(handle);
@@ -36,17 +36,17 @@ fn main() {
 #[cfg_attr(not(feature = "threads"), tokio::main(flavor = "current_thread"))]
 async fn main() {
     use futures::future::join_all;
-    use hyper::client::Client;
-    use hyper::Uri;
+    use hyper::client::{Client, connect::Connect};
+    use hyper::{Uri, body::HttpBody};
 
     async fn async_connect_and_read<C, B>(client: Client<C, B>)
     where
         C: Connect + Clone + Send + Sync + 'static,
         B: HttpBody + Send + 'static,
         B::Data: Send,
-        B::Error: Into<Box<dyn StdError + Send + Sync>>,
+        B::Error: Into<Box<dyn std::error::Error> + Send + Sync>>,
     {
-        let future = client.get(Uri::from_static(SERVER)).await.unwrap();
+        let _future = client.get(Uri::from_static(SERVER)).await.unwrap();
     }
 
     let client = Client::new();
--- benchmarks/tokio-startup/run.sh
+++ benchmarks/tokio-startup/run.sh
@@ -1,5 +1,5 @@
 cargo build --release
 for i in {1..10} 
 do
-    ./target/release/bin > "$ARTIFACTS_FOLDER/tokio-startup.txt"
+    ./target/release/bin >> "$ARTIFACTS_FOLDER/tokio-startup.txt"
 done
