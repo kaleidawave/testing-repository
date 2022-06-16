72995d20bb292a5b397ec93aa60e63647d2a243a
Ben <kaleidawave@gmail.com>
2022-06-16 15:58:40 +0100

    Just work!!!!

--- benchmarks/rust-threads-vs-async/run.sh
+++ benchmarks/rust-threads-vs-async/run.sh
@@ -5,19 +5,19 @@ brew install caddy
 cargo build --release
 mv target/release/bin sync
 
-cargo build --release --features threads
-mv target/release/bin threads
+# cargo build --release --features threads
+# mv target/release/bin threads
 
-cargo build --release --features async
-mv target/release/bin async
+# cargo build --release --features async
+# mv target/release/bin async
 
-cargo build --release --features async,threads
-mv target/release/bin async_threads
+# cargo build --release --features async,threads
+# mv target/release/bin async_threads
 
 caddy start
 
-hyperfine --warmup 3 ./sync ./threads ./async ./async_threads
+hyperfine --warmup 3 ./sync --show-output
 
-hyperfine --warmup 3 ./sync ./threads ./async ./async_threads > "$ARTIFACTS_FOLDER/hyperfine-output.txt"
+# hyperfine --warmup 3 ./sync ./threads ./async ./async_threads > "$ARTIFACTS_FOLDER/hyperfine-output.txt"
 
 caddy stop
