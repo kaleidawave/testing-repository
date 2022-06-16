cacbbfd0f06f6669c723f3cb11a432ed0a874cc8
Ben <kaleidawave@gmail.com>
2022-06-16 10:15:07 +0100

    Fixes v4

--- benchmarks/rust-threads-vs-async/run.sh
+++ benchmarks/rust-threads-vs-async/run.sh
@@ -8,6 +8,8 @@ do
   cp run.sh "files/$i" 
 done
 
+echo "Built files"
+
 cargo build --release
 mv target/release/rust-threads-vs-async sync
 
@@ -17,7 +19,9 @@ mv target/release/rust-threads-vs-async threads
 cargo build --release --features async
 mv target/release/rust-threads-vs-async async
 
-cargo build --release --features async threads
+cargo build --release --features async,threads
 mv target/release/rust-threads-vs-async async_threads
+
+echo "Running hyperfine"
 
 hyperfine --warmup 3 sync threads async async_threads > "$ARTIFACTS_FOLDER/hyperfine-output.txt"
