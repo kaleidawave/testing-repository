273b77b48412b7db8b8d60d30e343e36d0ed30d5
Ben <kaleidawave@gmail.com>
2022-08-19 12:06:50 +0100

    Deploy & get video

---
+++ one-off/mongo-db-deploy/src/lib.rs
@@ -0,0 +1,84 @@
+mod definitions;
+
+use definitions::{Requests, Responses, Todo};
+use mongodb::{Collection, Database, bson::{doc, oid::ObjectId}};
+use poem::{get, handler, Route, middleware::AddData, web::{Json, Data}, EndpointExt};
+
+#[handler]
+async fn index(Json(request): Json<Requests>, collection: Data<&Collection<Todo>>) -> Responses {
+    match request {
+        Requests::Create { title } => {
+            let res = collection
+                .insert_one(
+                    Todo { title, done: false },
+                    None,
+                )
+                .await;
+
+            Responses::CreatedTodoItem
+        }
+        Requests::UpdateTodoItems { done, undone } => {
+            for done in done {
+                collection.update_one(
+                    doc! {"_id": done },
+                    doc! {
+                        "done": true
+                    },
+                    None,
+                );
+            }
+
+            for undone in undone {
+                collection.update_one(
+                    doc! {"_id": undone },
+                    doc! {
+                        "done": false
+                    },
+                    None,
+                );
+            }
+
+            Responses::UpdatedTodoItems
+        }
+        Requests::ListTodoItems { include_done } => {
+            let filter = if include_done {
+                None
+            } else {
+                Some(doc! { "done": false })
+            };
+
+            let mut cursor = collection
+                .find(filter, None)
+                .await
+                .unwrap();
+
+            let mut items = Vec::new();
+            while let Ok(_) = cursor.advance().await {
+                let id: ObjectId = cursor.current().get_object_id("_id").unwrap();
+                let todo: Todo = cursor.deserialize_current().unwrap();
+                items.push((id, todo));
+            }
+
+            Responses::TodoItems { items }
+        }
+    }
+}
+
+#[shuttle_service::main]
+async fn main(
+    #[shared::MongoDb] db: Database,
+) -> shuttle_service::ShuttlePoem<impl poem::Endpoint> {
+    let collection = db.collection::<Todo>("todo_items");
+
+    let app = Route::new()
+        .at("/", get(index))
+        .with(AddData::new(collection));
+
+    Ok(app)
+}
+
+impl poem::IntoResponse for Responses {
+    fn into_response(self) -> poem::Response {
+        Json(self).into_response()
+    }
+}
