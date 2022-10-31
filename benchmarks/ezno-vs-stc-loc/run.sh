344056d632f28d7c55701a50eec8600b2d69a5f4
Ben <kaleidawave@gmail.com>
2022-10-31 11:11:48 +0000

    Fix key issue

--- benchmarks/ezno-vs-stc-loc/run.sh
+++ benchmarks/ezno-vs-stc-loc/run.sh
@@ -1,14 +1,25 @@
+echo "::group::Install LOC"
 cargo install loc
+echo "::endgroup::"
 
+echo "::group::Get STC"
 git clone https://github.com/dudykr/stc stc
+echo "::endgroup::"
 
-echo $SPECIAL_GH_TOKEN > gh auth login --with-token 
+echo "::group::Get EZNO"
 gh repo clone kaleidawave/ezno-private ezno
+echo "::endgroup::"
 
+echo "::group::Check both projects there"
 ls
+echo "::endgroup::"
 
+echo "::group::STC information"
 loc stc
 cat stc/README.md
+echo "::endgroup::"
 
+echo "::group::EZNO information"
 loc ezno
 cat ezno/README.md
+echo "::endgroup::"
