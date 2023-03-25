7a23f531703037e0a5e0038da6936a692e66d834
Ben <kaleidawave@gmail.com>
2023-03-25 11:21:46 +0000

    Download CC video

---
+++ video/script.py
@@ -0,0 +1,30 @@
+import os
+
+arguments = dict()
+
+with open("./video-requests.txt") as f:
+    counter = 0
+    for line in f:
+        line = line.strip()
+        commands = line.split(' ')
+        url = commands[0]
+
+        path = str(counter)
+        if commands[-1].startswith('='):
+            path = commands.pop()[1:]
+        counter += 1
+
+        arguments.setdefault(url, []).append([*commands[1:], path])
+
+print(arguments)
+
+for url, slices in arguments:
+    os.system(f"yt-dlp {url} -o output.mp4 -f mp4")
+    for slice in slices:
+        start = slice[0]
+        end = slice[1]
+        name = slice[2]
+        print(f"Snipping out {start} to {end}")
+        os.system("ffmpeg -i output.mp4 -ss \"{start}\" -to \"{end}\" -y cut-output.mp4")
+        os.system(f"mv cut-output.mp4 \"output-{name}.mp4\"")
+    os.remove("rm output.mp4")
