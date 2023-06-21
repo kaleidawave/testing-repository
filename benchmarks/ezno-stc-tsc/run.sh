7dabc12fe33ed67d1ae6e6aaa559a8352a0d806d
Ben <kaleidawave@gmail.com>
2023-06-21 13:52:57 +0100

    Homebrew + debug

--- benchmarks/ezno-stc-tsc/run.sh
+++ benchmarks/ezno-stc-tsc/run.sh
@@ -23,8 +23,10 @@ oxidation-compiler check demo.ts
 echo "TSC:"
 tsc demo.ts
 echo "STC:"
-./stc/target/release/stc demo.ts
+./stc/target/debug/stc demo.ts
+# ./stc/target/release/stc demo.ts
 echo "::endgroup::"
 
 # Run benchmark
-hyperfine -i 'oxidation-compiler check ./demo.ts' 'tsc demo.ts' 'stc/target/release/stc demo.ts'
+hyperfine -i 'oxidation-compiler check ./demo.ts' 'tsc demo.ts' 'stc/target/debug/stc demo.ts'
+# hyperfine -i 'oxidation-compiler check ./demo.ts' 'tsc demo.ts' 'stc/target/release/stc demo.ts'
