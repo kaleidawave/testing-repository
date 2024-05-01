7910d11908495583749c4d3322b11a641acaf583
Ben <kaleidawave@gmail.com>
2024-05-01 21:34:47 +0100

    wo

--- benchmarks/cargo-flamegraph-test/run.sh
+++ benchmarks/cargo-flamegraph-test/run.sh
@@ -1,15 +1,20 @@
-cat /proc/sys/kernel/perf_event_paranoid
+sudo cat /proc/sys/kernel/kptr_restrict
+sudo cat /proc/sys/kernel/perf_event_paranoid
 
 echo "::group::First run"
 export CARGO_PROFILE_RELEASE_DEBUG=true
 cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g"
 
-ls
+echo "<div>
+$(cat flamegraph.svg)
+</div>" >> $GITHUB_STEP_SUMMARY
 
 echo "::endgroup"
 
 echo "::group::More runs"
-cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g" -o artifacts/one.svg
-cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g" -o artifacts/two.svg
-cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g" --flamechart -o artifacts/three.svg
+cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g" -o ./artifacts/one.svg
+cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g" -o ./artifacts/two.svg
+cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g" --flamechart -o ./artifacts/three.svg
 echo "::endgroup"
+
+ls artifacts
