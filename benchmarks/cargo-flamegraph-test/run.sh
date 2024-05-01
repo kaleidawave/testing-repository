266b1efa5e6ee4686f013c311913795968e30a3d
Ben <kaleidawave@gmail.com>
2024-05-01 21:28:01 +0100

    okay once more

--- benchmarks/cargo-flamegraph-test/run.sh
+++ benchmarks/cargo-flamegraph-test/run.sh
@@ -1,13 +1,15 @@
+cat /proc/sys/kernel/perf_event_paranoid
+
 echo "::group::First run"
 export CARGO_PROFILE_RELEASE_DEBUG=true
-cargo flamegraph
+cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g"
 
 ls
 
 echo "::endgroup"
 
 echo "::group::More runs"
-cargo flamegraph -o artifacts/one.svg
-cargo flamegraph -o artifacts/two.svg
-cargo flamegraph --flamechart -o artifacts/three.svg
+cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g" -o artifacts/one.svg
+cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g" -o artifacts/two.svg
+cargo flamegraph -c "record -o perf.data -F997 --call-graph dwarf,16384 -g" --flamechart -o artifacts/three.svg
 echo "::endgroup"
