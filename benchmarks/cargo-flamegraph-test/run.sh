9c7c4d021605d094660392788a76d8cf785d74d2
Ben <kaleidawave@gmail.com>
2024-05-01 21:18:44 +0100

    Final try at trying to something

--- benchmarks/cargo-flamegraph-test/run.sh
+++ benchmarks/cargo-flamegraph-test/run.sh
@@ -1,7 +1,13 @@
+echo "::group::First run"
+export CARGO_PROFILE_RELEASE_DEBUG=true
 cargo flamegraph
 
 ls
 
+echo "::endgroup"
+
+echo "::group::More runs"
 cargo flamegraph -o artifacts/one.svg
 cargo flamegraph -o artifacts/two.svg
 cargo flamegraph --flamechart -o artifacts/three.svg
+echo "::endgroup"
--- benchmarks/cargo-flamegraph-test/setup.sh
+++ benchmarks/cargo-flamegraph-test/setup.sh
@@ -2,4 +2,7 @@ cargo install flamegraph
 
 sudo apt install linux-tools-common linux-tools-generic linux-tools-`uname -r`
 
-sudo sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'
+# sudo sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'
+sudo sysctl -w kernel.perf_event_paranoid=1
+
+cat /proc/sys/kernel/perf_event_paranoid
