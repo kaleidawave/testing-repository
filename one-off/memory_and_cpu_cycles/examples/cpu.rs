3ed540c0b2c33aaca66ff42d626a2b075800e433
Ben <kaleidawave@gmail.com>
2024-09-09 20:29:16 +0100

    valgrind stuff

---
+++ one-off/memory_and_cpu_cycles/examples/cpu.rs
@@ -0,0 +1,20 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+    let to_run: usize = args.first().ok_or("expected memory size")?.parse()?;
+
+    std::hint::black_box(run(to_run));
+
+    Ok(())
+}
+
+#[inline(never)]
+fn run(count: usize) {
+    for _ in 0..count {
+        call();
+    }
+}
+
+#[inline(never)]
+fn call() {
+    eprintln!("Hello World!");
+}
---
+++ one-off/memory_and_cpu_cycles/examples/memory.rs
@@ -0,0 +1,13 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+    let to_allocate: usize = args.first().ok_or("expected memory size")?.parse()?;
+
+    let vec = vec![0u8; to_allocate];
+
+    eprintln!("Allocated {} bytes", vec.len());
+    std::hint::black_box(vec);
+
+    std::thread::sleep(std::time::Duration::from_secs(10));
+
+    Ok(())
+}
