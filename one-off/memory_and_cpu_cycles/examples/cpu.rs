343d558ba5ea93842541e66dd96332db1a8d3e2a
Ben <kaleidawave@gmail.com>
2024-09-21 11:36:55 +0100

    Few more tests

--- one-off/memory_and_cpu_cycles/examples/cpu.rs
+++ one-off/memory_and_cpu_cycles/examples/cpu.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = std::env::args().skip(1).collect::<Vec<_>>();
-    let to_run: usize = args.first().ok_or("expected memory size")?.parse()?;
+    let to_run: usize = args.first().ok_or("expected run count")?.parse()?;
 
     std::hint::black_box(run(to_run));
 
