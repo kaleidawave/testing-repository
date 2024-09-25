e0473abcef76948ea22b34f89b8020e8cc870494
Ben <kaleidawave@gmail.com>
2024-09-25 23:40:31 +0100

    More testing

--- one-off/simd_test/examples/non_simd.rs
+++ one-off/simd_test/examples/non_simd.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let bytes: Vec<u8> = fs::read("example")?;
-    let mut data = [0u32; 4];
+    let mut data = [0u32; 8];
     for byte in bytes {
         match byte {
             b'(' => {
@@ -16,6 +16,18 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
             }
             b'>' => {
                 data[3] += 1;
+            }
+            b'a' => {
+                data[4] += 1;
+            }
+            b'b' => {
+                data[5] += 1;
+            }
+            b'c' => {
+                data[6] += 1;
+            }
+            b'd' => {
+                data[7] += 1;
             }
             _ => {}
         }
--- one-off/simd_test/examples/simd.rs
+++ one-off/simd_test/examples/simd.rs
@@ -1,13 +1,13 @@
 #![feature(portable_simd)]
-use std::simd::{u8x4, cmp::SimdPartialEq};
+use std::simd::{u8x8, cmp::SimdPartialEq};
 use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let bytes: Vec<u8> = fs::read("example")?;
-    let mut data = [0u32; 4];
-    let a = u8x4::from_slice(b"()<>");
+    let mut data = [0u32; 8];
+    let a = u8x8::from_slice(b"()<>abcd");
     for byte in bytes {
-        let chr_simd = u8x4::splat(byte);
+        let chr_simd = u8x8::splat(byte);
         let result = a.simd_eq(chr_simd);
         if let Some(idx) = result.first_set() {
             data[idx] += 1;
