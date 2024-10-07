4186a337b003120910098fa61557e22dd2224b34
Ben <kaleidawave@gmail.com>
2024-10-07 13:13:29 +0100

    Update benchmarks

--- benchmarks/type-checkers/run.sh
+++ benchmarks/type-checkers/run.sh
@@ -22,80 +22,81 @@ $OUTPUT
 } 
 
 run_tool "Ezno" "./ezno/target/release/ezno check demo.tsx --timings"
-run_tool "Ezno (no diagnostics printing)" "./ezno/target/release/ezno check demo.tsx --max-diagnostics 0 --timings"
-run_tool "TSC" "tsc --pretty --noEmit --jsx preserve --noLib --diagnostics demo.tsx overrides.d.ts"
-run_tool "STC" "./stc/target/release/stc test demo.tsx"
+run_tool "Ezno (next)" "./ezno-next/target/release/ezno check demo.tsx --timings"
+# run_tool "Ezno (no diagnostics printing)" "./ezno/target/release/ezno check demo.tsx --max-diagnostics 0 --timings"
+run_tool "TSC" "tsc --pretty --noEmit --noLibCheck --jsx preserve --diagnostics demo.tsx"
+# run_tool "STC" "./stc/target/release/stc test demo.tsx"
 
 echo "::endgroup::"
 
-echo "::group::Run benchmarks"
+# echo "::group::Run benchmarks"
 
-echo "## Benchmark files" >> $GITHUB_STEP_SUMMARY
+# echo "## Benchmark files" >> $GITHUB_STEP_SUMMARY
 
 # Ezno and TSC
-echo "\`\`\`
-$(hyperfine -i './ezno/target/release/ezno check ./demo.tsx' 'tsc --pretty --noEmit --jsx preserve demo.tsx')
-\`\`\`" >> $GITHUB_STEP_SUMMARY
+# echo "\`\`\`
+# $(hyperfine -i './ezno/target/release/ezno check ./demo.tsx' 'tsc --pretty --noEmit --jsx preserve demo.tsx')
+# \`\`\`" >> $GITHUB_STEP_SUMMARY
 
-# Ezno, STC and TSC
-H1=$(hyperfine -i './ezno/target/release/ezno check ./demo.tsx' './stc/target/release/stc test demo.tsx' 'tsc --pretty --noEmit --jsx preserve demo.tsx')
+# # Ezno, STC and TSC
+# H1=$(hyperfine -i './ezno/target/release/ezno check ./demo.tsx' './stc/target/release/stc test demo.tsx' 'tsc --pretty --noEmit --jsx preserve demo.tsx')
 
-echo "with STC
-\`\`\`
-$H1
-\`\`\`" >> $GITHUB_STEP_SUMMARY
+# echo "with STC
+# \`\`\`
+# $H1
+# \`\`\`" >> $GITHUB_STEP_SUMMARY
 
-echo "Given demo.tsx with $(wc -l < demo.tsx) lines & large.tsx with $(wc -l < large.tsx) lines" >> $GITHUB_STEP_SUMMARY
+# echo "Given demo.tsx with $(wc -l < demo.tsx) lines & large.tsx with $(wc -l < large.tsx) lines" >> $GITHUB_STEP_SUMMARY
 
-echo "# simple.tsx
-$(./ezno/target/release/ezno check ./simple.tsx --max-diagnostics 0 --timings)
+# echo "# simple.tsx
+# $(./ezno/target/release/ezno check ./simple.tsx --max-diagnostics 0 --timings)
 
-# demo.tsx
-$(./ezno/target/release/ezno check ./demo.tsx --max-diagnostics 0 --timings)
+# # demo.tsx
+# $(./ezno/target/release/ezno check ./demo.tsx --max-diagnostics 0 --timings)
 
-# large.tsx
-$(./ezno/target/release/ezno check ./large.tsx --max-diagnostics 0 --timings)
+# # large.tsx
+# $(./ezno/target/release/ezno check ./large.tsx --max-diagnostics 0 --timings)
 
-# comparison of small vs large (ezno)
-$(hyperfine -i './ezno/target/release/ezno check ./simple.tsx' './ezno/target/release/ezno check ./demo.tsx' './ezno/target/release/ezno check ./large.tsx')" > "$ARTIFACTS_FOLDER/ezno-diff.txt"
+# # comparison of small vs large (ezno)
+# $(hyperfine -i './ezno/target/release/ezno check ./simple.tsx' './ezno/target/release/ezno check ./demo.tsx' './ezno/target/release/ezno check ./large.tsx')" > "$ARTIFACTS_FOLDER/ezno-diff.txt"
 
-echo "# simple.tsx
-$(tsc --pretty --diagnostics --noEmit --noLib --jsx preserve ./simple.tsx overrides.d.ts)
+# echo "# simple.tsx
+# $(tsc --pretty --diagnostics --noEmit --noLib --jsx preserve ./simple.tsx overrides.d.ts)
 
-# demo.tsx
-$(tsc --pretty --diagnostics --noEmit --noLib --jsx preserve ./demo.tsx overrides.d.ts)
+# # demo.tsx
+# $(tsc --pretty --diagnostics --noEmit --noLib --jsx preserve ./demo.tsx overrides.d.ts)
 
-# large.tsx
-$(tsc --pretty --diagnostics --noEmit --noLib --jsx preserve ./large.tsx overrides.d.ts)
+# # large.tsx
+# $(tsc --pretty --diagnostics --noEmit --noLib --jsx preserve ./large.tsx overrides.d.ts)
 
-# comparison of simple vs small vs large (tsc)
-$(hyperfine -i 'tsc --noEmit --noLib --pretty --jsx preserve simple.tsx overrides.d.ts' 'tsc --noEmit --noLib --pretty --jsx preserve demo.tsx overrides.d.ts' 'tsc --noEmit --noLib --pretty --jsx preserve large.tsx overrides.d.ts')" > "$ARTIFACTS_FOLDER/tsc-diff.txt"
+# # comparison of simple vs small vs large (tsc)
+# $(hyperfine -i 'tsc --noEmit --noLib --pretty --jsx preserve simple.tsx overrides.d.ts' 'tsc --noEmit --noLib --pretty --jsx preserve demo.tsx overrides.d.ts' 'tsc --noEmit --noLib --pretty --jsx preserve large.tsx overrides.d.ts')" > "$ARTIFACTS_FOLDER/tsc-diff.txt"
 
-# Ezno, STC and TSC on large
-H2=$(hyperfine -i './ezno/target/release/ezno check ./large.tsx' './stc/target/release/stc test large.tsx' 'tsc --pretty --noEmit --noLib --jsx preserve large.tsx overrides.d.ts')
+# # Ezno, STC and TSC on large
+# H2=$(hyperfine -i './ezno/target/release/ezno check ./large.tsx' './stc/target/release/stc test large.tsx' 'tsc --pretty --noEmit --noLib --jsx preserve large.tsx overrides.d.ts')
 
-echo "On large
-\`\`\`
-$H2
-\`\`\`" >> $GITHUB_STEP_SUMMARY
+# echo "On large
+# \`\`\`
+# $H2
+# \`\`\`" >> $GITHUB_STEP_SUMMARY
 
-echo "::endgroup::"
+# echo "::endgroup::"
 
-echo "::group::Each"
-for f in "./all"
-do
-if [[ "$f" != *\.* ]]
-then
-  echo "Item: $f" >> all.txt
-  ./ezno/target/release/ezno check $f --max-diagnostics 0 --timings >> all.txt
-fi
-done
+# echo "::group::Each"
+# for f in "./all"
+# do
+# if [[ "$f" != *\.* ]]
+# then
+#   echo "Item: $f" >> all.txt
+#   ./ezno/target/release/ezno check $f --max-diagnostics 0 --timings >> all.txt
+# fi
+# done
 
-cp ./all.txt $ARTIFACTS_FOLDER
+# cp ./all.txt $ARTIFACTS_FOLDER
 
-ls $ARTIFACTS_FOLDER
+# ls $ARTIFACTS_FOLDER
 
-echo "::endgroup::"
+# echo "::endgroup::"
 
 # Temporary what is going on
 # cat $GITHUB_STEP_SUMMARY >> "$ARTIFACTS_FOLDER/out.md"
--- benchmarks/type-checkers/setup.sh
+++ benchmarks/type-checkers/setup.sh
@@ -5,20 +5,20 @@ echo "::group::Get tools"
 
 # TODO flow, hegel, install tools (or cache better), more
 
-echo "::group::Build STC"
+# echo "::group::Build STC"
 
-rustup toolchain install nightly-2023-06-20
+# rustup toolchain install nightly-2023-06-20
 
-git clone https://github.com/dudykr/stc stc
-cd stc
-git reset --hard 693cf5a891c5580542811b906616f0c15d0dd0fc
-cd ..
+# git clone https://github.com/dudykr/stc stc
+# cd stc
+# git reset --hard 693cf5a891c5580542811b906616f0c15d0dd0fc
+# cd ..
 
-cargo +nightly-2023-06-20 build --manifest-path stc/crates/stc/Cargo.toml --no-default-features --locked
+# cargo +nightly-2023-06-20 build --manifest-path stc/crates/stc/Cargo.toml --no-default-features --locked
 
-./stc/target/release/stc --help
+# ./stc/target/release/stc --help
 
-echo "::endgroup::"
+# echo "::endgroup::"
 
 echo "::group::Build Ezno (and demo.tsx)"
 rustc --version
@@ -32,42 +32,47 @@ rustup toolchain install stable
 # mv ezno2 ezno
 # ls ezno
 
-# -b main
-git clone https://github.com/kaleidawave/ezno.git ezno
+# Main
+git clone https://github.com/kaleidawave/ezno.git ezno -b main
 
 cargo build --manifest-path ezno/Cargo.toml --release --bin ezno
+
+# and new parser
+git clone https://github.com/kaleidawave/ezno.git ezno-next -b merge-lexer
+cargo build --manifest-path ezno-next/Cargo.toml --release --bin ezno
 
 # cargo install --path ezno
 
 ./ezno/target/release/ezno info
+./ezno/target/release/ezno-next info
 
 echo "::group::Build demo files"
 cargo run --manifest-path ezno/Cargo.toml -p ezno-parser --example code_blocks_to_script ./ezno/checker/specification/specification.md --comment-headers --out ./demo.tsx
 
 cp ./demo.tsx $ARTIFACTS_FOLDER
 
-# Simple
-echo "const x: string = 4;" >> simple.tsx
+# # Simple
+# echo "const x: string = 4;" >> simple.tsx
 
-# Large
-for i in {1..10}; do
-    cat ./demo.tsx >> ./large.tsx
-done
+# # Large
+# for i in {1..10}; do
+#     cat ./demo.tsx >> ./large.tsx
+# done
 
-echo "interface Array {}
-interface Boolean {}
-interface Function {}
-interface IArguments {}
-interface Number {}
-interface Object {}
-interface RegExp {}
-interface String {}" > overrides.d.ts
+# echo "interface Array {}
+# interface Boolean {}
+# interface Function {}
+# interface IArguments {}
+# interface Number {}
+# interface Object {}
+# interface RegExp {}
+# interface String {}" > overrides.d.ts
 
-mkdir all
-cargo run --manifest-path ezno/Cargo.toml -p ezno-parser --example code_blocks_to_script ./ezno/checker/specification/specification.md --into-files all ts
+# mkdir all
+# cargo run --manifest-path ezno/Cargo.toml -p ezno-parser --example code_blocks_to_script ./ezno/checker/specification/specification.md --into-files all ts
 
-ls all
-# --repeat 50
+# ls all
+# # --repeat 50
 
 echo "::endgroup::"
 
