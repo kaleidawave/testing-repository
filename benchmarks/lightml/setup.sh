611fad2aaf21663e38286d65dbefadf7b15138b4
Ben <kaleidawave@gmail.com>
2025-02-04 10:09:41 +0000

    RIPGREP

--- benchmarks/lightml/setup.sh
+++ benchmarks/lightml/setup.sh
@@ -4,10 +4,13 @@ sudo apt-get install valgrind
 # curl --proto '=https' --tlsv1.2 -LsSf https://github.com/mstange/samply/releases/download/samply-v0.13.1/samply-installer.sh | sh
 
 # Utilities
+brew install ripgrep
+
 gh release download -R BurntSushi/ripgrep --pattern "*x86*unknown-linux*"
 # TODO can we reuse the name here
 unzip ripgrep-14.1.1-x86_64-unknown-linux-musl
-echo ripgrep-14.1.1-x86_64-unknown-linux-musl >> "$GITHUB_PATH"
+# echo ripgrep-14.1.1-x86_64-unknown-linux-musl >> "$GITHUB_PATH"
+ls -R
 
 # Check they exist
 # samply --help
