8a2e1c674af88515e55ee1eceeb455b33fc3b945
Ben <kaleidawave@gmail.com>
2025-02-04 09:13:02 +0000

    Getting there

--- benchmarks/lightml/run.sh
+++ benchmarks/lightml/run.sh
@@ -1,8 +1,20 @@
-./target/release/example &> $GITHUB_STEP_SUMMARY
+CODE_FENCE='```'
+OUTPUT="
+### Output
+"$CODE_FENCE"shell
+$(./target/release/example)
+$CODE_FENCE
 
-hyperfine ./target/release/example &> $GITHUB_STEP_SUMMARY
+### Hyperfine
+"$CODE_FENCE"shell
+$(hyperfine ./target/release/example)
+$CODE_FENCE"
+
+echo "$OUTPUT" > $GITHUB_STEP_SUMMARY
 
 valgrind --log-file="$ARTIFACTS_FOLDER/out-mem.txt" ./target/release/example
 valgrind --tool=callgrind --callgrind-out-file="$ARTIFACTS_FOLDER/out-cpu.txt" ./target/release/example
+
+ls $ARTIFACTS_FOLDER
 
 # samply record -s -o "$ARTIFACTS_FOLDER/out-samply.json.gz" ./target/release/example
