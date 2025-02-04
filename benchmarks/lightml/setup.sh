0207f36937c4bff9d92b20d7b965f8451b330fd7
Ben <kaleidawave@gmail.com>
2025-02-04 10:02:45 +0000

    Try again

--- benchmarks/lightml/setup.sh
+++ benchmarks/lightml/setup.sh
@@ -3,6 +3,12 @@ brew install hyperfine
 sudo apt-get install valgrind
 # curl --proto '=https' --tlsv1.2 -LsSf https://github.com/mstange/samply/releases/download/samply-v0.13.1/samply-installer.sh | sh
 
+# Utilities
+gh release download -R BurntSushi/ripgrep --pattern "*x86*unknown-linux*"
+# TODO can we reuse the name here
+unzip ripgrep-14.1.1-x86_64-unknown-linux-musl
+echo ripgrep-14.1.1-x86_64-unknown-linux-musl >> "$GITHUB_PATH"
+
 # Check they exist
 # samply --help
 # valgrind --help
@@ -10,8 +16,8 @@ sudo apt-get install valgrind
 # Example file
 curl https://www.theguardian.com/uk > ./example.html
 
-# # Build (debug) binary
-# cargo build
+# Build (debug) binary
+cargo build
 
 # Build (release) binary
 cargo build --release
