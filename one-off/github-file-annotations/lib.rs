1192a59877949f3afa8b133b57db1c0023429ca8
Ben <kaleidawave@gmail.com>
2025-02-05 18:31:04 +0000

    More

--- one-off/github-file-annotations/lib.rs
+++ one-off/github-file-annotations/lib.rs
@@ -3,10 +3,9 @@ pub fn add(left: u64, right: u64) -> u64 {
 }
 
 fn it_works() {
-    let result = add(2, "test");
-    assert_eq!(result, 4);
+    let result1 = add(2, "test");
+    assert_eq!(result1, 4);
 
     let result2 = add(2, "bad");
-
     assert7!();
 }
--- one-off/github-file-annotations/run.ts
+++ one-off/github-file-annotations/run.ts
@@ -9,13 +9,13 @@ for await (const line of out) {
         const out = JSON.parse(line);
         if (out.message?.code) {
             const first = out.message.spans[0];
-            const name = first.file_name;
+            const file = `one-off/github-file-annotations/${first.file_name}`;
             const line = first.line_start;
             const endLine = first.line_end;
             const col = first.column_start;
             const colEnd = first.column_end;
             const message = first.label;
-            console.log(`::error file=${name},line=${line},endLine=${endLine},col=${col},colEnd=${colEnd}::'${message}'`);
+            console.log(`::error file=${file},line=${line},endLine=${endLine},col=${col},colEnd=${colEnd}::'${message}'`);
         }
     }
 }
