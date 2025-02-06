1ca5d03fefdbdc2da1b34df03e22a932d05abae9
Ben <kaleidawave@gmail.com>
2025-02-06 13:27:02 +0000

    just work

--- one-off/memory_and_cpu_cycles/examples/program_exit_code.rs
+++ one-off/memory_and_cpu_cycles/examples/program_exit_code.rs
@@ -5,7 +5,7 @@ fn program(_i: i32) {}
 
 fn main() -> ExitCode {
     let args = std::env::args().skip(1).collect::<Vec<_>>();
-    let to_allocate: usize = args.first().ok_or("expected memory size")?.parse()?;
+    let to_allocate: usize = args.first().ok_or("expected memory size").unwrap().parse().unwrap();
 
     let vec = vec![0u8; to_allocate];
 
