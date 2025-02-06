f591db87f01d91d32be4f52d5150918df15374a7
Ben <kaleidawave@gmail.com>
2025-02-06 13:31:39 +0000

    Even GH is giving up

--- one-off/memory_and_cpu_cycles/examples/program_exit_code.rs
+++ one-off/memory_and_cpu_cycles/examples/program_exit_code.rs
@@ -1,13 +1,21 @@
-use std::hint::black_box;
 use std::process::ExitCode;
 
-fn program(_i: i32) {}
+fn program(_i: usize) {}
 
 fn main() -> ExitCode {
     let args = std::env::args().skip(1).collect::<Vec<_>>();
-    let to_allocate: usize = args.first().ok_or("expected memory size").unwrap().parse().unwrap();
+    let to_allocate: usize = args
+        .first()
+        .ok_or("expected memory size")
+        .unwrap()
+        .parse()
+        .unwrap();
 
     let vec = vec![0u8; to_allocate];
+
+    for i in 0..to_allocate {
+        std::hint::black_box(program(i));
+    }
 
     eprintln!("Allocated {} bytes", vec.len());
     std::hint::black_box(vec);
