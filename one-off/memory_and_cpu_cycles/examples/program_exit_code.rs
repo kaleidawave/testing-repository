10bb079c05cad860c41d20396c5d2d05cf7f7702
Ben <kaleidawave@gmail.com>
2025-02-06 13:04:30 +0000

    Try to get to bottom of valgrind

---
+++ one-off/memory_and_cpu_cycles/examples/program_exit_code.rs
@@ -0,0 +1,12 @@
+use std::process::ExitCode;
+use std::hint::black_box;
+
+fn program(_i: i32) {}
+
+fn main() -> ExitCode {
+    for i in 0..10 {
+        black_box(program(i));
+    }
+    black_box(vec![0u8; 1000]);
+    ExitCode::FAILURE
+}
