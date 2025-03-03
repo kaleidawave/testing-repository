f7d73d224884e2a58f07c895051fb084ac495391
Ben <kaleidawave@gmail.com>
2025-03-03 09:49:47 +0000

    Updates (count time, skip _FIXTURE, correct files)

--- one-off/test262/main.rs
+++ one-off/test262/main.rs
@@ -1,8 +1,6 @@
-use std::fs::{read_dir, read_to_string, write};
-use std::path::{Path, PathBuf};
-// use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
-// use std::io::Write;
 use std::fmt::Write;
+use std::fs::{create_dir, read_dir, read_to_string, write};
+use std::path::{Path, PathBuf};
 
 fn main() {
     let path: &Path = &PathBuf::from("./test262/test");
@@ -11,29 +9,37 @@ fn main() {
 
     let mut completed = 0;
     let mut successful = 0;
-    let mut expected_invalid_parses = 0;
+
+    let now = std::time::Instant::now();
+
     visit_dirs(path, &mut |path| {
         if let Some("js") = path.extension().and_then(std::ffi::OsStr::to_str) {
+            if let Some(path) = path.file_name().and_then(std::ffi::OsStr::to_str) {
+                if path.contains("_FIXTURE") {
+                    return;
+                }
+            }
+
             let Ok(source) = read_to_string(path) else {
                 eprintln!("Could not read {path}", path = path.display());
                 return;
             };
 
             let Some(start) = source.find("/*---") else {
-                eprintln!("No /*--- under {path}", path = path.display());
+                eprintln!("No '/*---' under {path}", path = path.display());
                 return;
             };
             let start = start + "/*---".len();
 
             let remaining = &source[start..];
             let Some(end) = remaining.find("---*/") else {
-                eprintln!("No ---*/ under {path}", path = path.display());
+                eprintln!("No '---*/' under {path}", path = path.display());
                 return;
             };
 
             let metadata = &remaining[..end];
 
-            let end = end + "/*---".len();
+            let end = end + "---*/".len();
             let code = &remaining[end..];
 
             let mut should_not_parse = false;
@@ -62,52 +68,48 @@ fn main() {
                 }
             };
 
-            if should_not_parse {
-                expected_invalid_parses += 1;
-            }
-
             let result = <ezno_parser::Module as ezno_parser::ASTNode>::from_string_with_options(
                 code.into(),
                 Default::default(),
                 None,
             );
 
-            match result {
+            let _ = match result {
                 Ok(_) if should_not_parse => writeln!(
                     &mut invalid,
                     "js-parse   {path} (should not parse)",
                     path = path.display()
-                )
-                .unwrap(),
+                ),
                 Err(_) if !should_not_parse => writeln!(
                     &mut invalid,
-                    "js-parse   {path} (should parse but errored)",
+                    "js-parse   {path} (should parse but error'ed)",
                     path = path.display()
-                )
-                .unwrap(),
+                ),
                 _ => {
+                    successful += 1;
                     writeln!(
                         &mut valid,
                         "js-parse   {path} ✅✅✅",
                         path = path.display()
-                    );
-                    successful += 1;
+                    )
                 }
-            }
+            };
 
             completed += 1;
         } else {
-            eprintln!("Non test file {path}", path = path.display());
+            eprintln!("Not a test file: {path}", path = path.display());
         }
     });
 
     eprintln!(
-        "Completed {completed} files. {expected_invalid_parses} tests for bad parsing. {successful} successful passes. {errors} fails",
-        errors = completed - successful
+        "Completed {completed} files in {duration:?}. {successful} successful passes. {errors} fails",
+        errors = completed - successful,
+        duration = now.elapsed()
     );
 
-    write("valid.txt", valid).unwrap();
-    write("invalid.txt", invalid).unwrap();
+    let _ = create_dir("out");
+    write("out/valid.txt", valid).unwrap();
+    write("out/invalid.txt", invalid).unwrap();
 }
 
 fn visit_dirs(path: &Path, cb: &mut impl FnMut(&Path)) {
