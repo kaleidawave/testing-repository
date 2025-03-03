449de8fe7203ca941aa2520438811ed2023e5c29
Ben <kaleidawave@gmail.com>
2025-03-03 21:23:51 +0000

    Improve things inlcuding writing to a sqlite database

--- one-off/test262/main.rs
+++ one-off/test262/main.rs
@@ -1,25 +1,39 @@
-use std::fmt::Write;
-use std::fs::{create_dir, read_dir, read_to_string, write};
+use std::borrow::Cow;
+use std::fs::{create_dir, read_dir, read_to_string};
 use std::path::{Path, PathBuf};
 
 fn main() {
     let path: &Path = &PathBuf::from("./test262/test");
-    let mut valid = String::new();
-    let mut invalid = String::new();
 
     let mut completed = 0;
     let mut successful = 0;
 
     let now = std::time::Instant::now();
 
-    visit_dirs(path, &mut |path| {
-        if let Some("js") = path.extension().and_then(std::ffi::OsStr::to_str) {
-            if let Some(path) = path.file_name().and_then(std::ffi::OsStr::to_str) {
-                if path.contains("_FIXTURE") {
-                    return;
-                }
-            }
+    let _ = create_dir("out");
+    let connection = sqlite::open("out/database.db").unwrap();
 
+    let query = "CREATE TABLE results (
+    path        TEXT PRIMARY KEY,
+    info        TEXT,
+    negative    INTEGER,
+    code        TEXT,
+    pass        INTEGER,
+    parser_out  TEXT
+);";
+    connection.execute(query).unwrap();
+
+    let query = "INSERT INTO results VALUES (:path, :info, :negative, :code, :pass, :parser_out)";
+    let mut statement = connection.prepare(query).unwrap();
+
+    visit_dirs(path, &mut |path| {
+        if let Some(path) = path.file_name().and_then(std::ffi::OsStr::to_str) {
+            if path.contains("_FIXTURE") {
+                return;
+            }
+        }
+
+        if let Some("js") = path.extension().and_then(std::ffi::OsStr::to_str) {
             let Ok(source) = read_to_string(path) else {
                 eprintln!("Could not read {path}", path = path.display());
                 return;
@@ -42,28 +56,33 @@ fn main() {
             let end = end + "---*/".len();
             let code = &remaining[end..];
 
+            // Set by metadata
             let mut should_not_parse = false;
+            let mut info = "";
+
             {
                 let result = simple_yaml_parser::parse(metadata, |key, value| {
+                    // TODO description. negative.type, flags, locale
                     if let (
                         &[
                             simple_yaml_parser::YAMLKey::Slice("negative"),
                             simple_yaml_parser::YAMLKey::Slice("phase"),
                         ],
                         simple_yaml_parser::RootYAMLValue::String("parse"),
-                    ) = (key, value)
+                    ) = (key, &value)
                     {
                         should_not_parse = true;
+                    } else if let (
+                        &[simple_yaml_parser::YAMLKey::Slice("info")],
+                        simple_yaml_parser::RootYAMLValue::String(content),
+                    ) = (key, value)
+                    {
+                        info = content;
                     }
                 });
 
                 if let Err(err) = result {
-                    writeln!(
-                        &mut invalid,
-                        "yaml-parse {path} {err:?}",
-                        path = path.display()
-                    )
-                    .unwrap();
+                    eprintln!("yaml-parse {path} {err:?}", path = path.display());
                     return;
                 }
             };
@@ -74,26 +93,37 @@ fn main() {
                 None,
             );
 
-            let _ = match result {
-                Ok(_) if should_not_parse => writeln!(
-                    &mut invalid,
-                    "js-parse   {path} (should not parse)",
-                    path = path.display()
-                ),
-                Err(_) if !should_not_parse => writeln!(
-                    &mut invalid,
-                    "js-parse   {path} (should parse but error'ed)",
-                    path = path.display()
-                ),
+            let (matched, reason) = match result {
+                Ok(_) if should_not_parse => {
+                    (false, Cow::Borrowed("parsed when should have failed"))
+                }
+                Err(error) if !should_not_parse => (false, Cow::Owned(error.reason)),
                 _ => {
                     successful += 1;
-                    writeln!(
-                        &mut valid,
-                        "js-parse   {path} ✅✅✅",
-                        path = path.display()
-                    )
+                    // TODO should emit -> parse -> emit and check results (roundtrip)
+                    // TODO should type check
+                    (true, Cow::Borrowed(""))
                 }
             };
+
+            {
+                // let query = "INSERT INTO results VALUES (:path, :info, :negative, :code, :pass, :parser_out)";
+                let values = &[
+                    (":path", path.display().to_string().into()),
+                    (":info", info.into()),
+                    (":negative", (should_not_parse as i64).into()),
+                    // space saving measure
+                    (":code", (if matched { "" } else { code }).into()),
+                    (":pass", (matched as i64).into()),
+                    (":parser_out", (&*reason).into()),
+                ];
+                statement
+                    .bind::<&[(_, sqlite::Value)]>(values)
+                    .expect("Could not bind");
+
+                while let Ok(sqlite::State::Row) = statement.next() {}
+                let _ = statement.reset();
+            }
 
             completed += 1;
         } else {
@@ -102,14 +132,13 @@ fn main() {
     });
 
     eprintln!(
-        "Completed {completed} files in {duration:?}. {successful} successful passes. {errors} fails",
+        "Completed {completed} tests in {duration:?}. {successful} successful passes. {errors} fails",
         errors = completed - successful,
         duration = now.elapsed()
     );
 
-    let _ = create_dir("out");
-    write("out/valid.txt", valid).unwrap();
-    write("out/invalid.txt", invalid).unwrap();
+    // write("out/valid.txt", valid).unwrap();
+    // write("out/invalid.txt", invalid).unwrap();
 }
 
 fn visit_dirs(path: &Path, cb: &mut impl FnMut(&Path)) {
